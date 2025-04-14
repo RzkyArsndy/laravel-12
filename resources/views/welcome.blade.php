fd0b1001f59645935881e1e894f002fac10c852f
RzkyArsndyy <rizkyarisandyy09@gmail.com>
2025-04-14 10:35:22 +0800

    checkpoint 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,10 +9,10 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>Rapor Kesehatan Digital</title>
 
-    <!-- Custom fonts for this template-->
-    <link href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+<!-- Custom fonts for this template-->
+<link href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
@@ -737,8 +737,8 @@
         </div>
     </div>
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="{{ asset('assets/vendor/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap core JavaScript-->
+<script src="{{ asset('assets/vendor/jquery/jquery.min.js')}}"></script>
     <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
     <!-- Core plugin JavaScript-->
@@ -753,7 +753,6 @@
     <!-- Page level custom scripts -->
     <script src="{{ asset('assets/js/demo/chart-area-demo.js')}}"></script>
     <script src="{{ asset('assets/js/demo/chart-pie-demo.js')}}"></script>
-
 </body>
 
 </html>
